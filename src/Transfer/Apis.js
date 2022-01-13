b9109b455b338f5a8fa0559c2b8951c978b5574d
camkaley <camkaley@gmail.com>
2022-01-13 19:42:24 +1100

    Added button

--- src/Transfer/Apis.js
+++ src/Transfer/Apis.js
@@ -1,16 +1,27 @@
 import Axios from "axios";
 const url = "http://localhost:4000";
 
-export function postInsult(insult) {
-  var payload = { insult: insult };
-
+export function getCount() {
   return new Promise((resolve, reject) => {
-    Axios.post(`${url}/register`, payload)
+    Axios.get(`${url}/getCount`)
       .then(function (response) {
-        resolve(response)
+        resolve(response.data)
       })
       .catch(function (error) {
         reject(error);
       });
   });
 }
+
+export function increment() {
+  return new Promise((resolve, reject) => {
+    Axios.get(`${url}/increment`)
+      .then(function (response) {
+        resolve(response.data)
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
+}
+
--- src/View/landing.js
+++ src/View/landing.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../Components/Display/header";
 import appConfig from "../Config/appConfig.json";
 import SlickText from "../Components/Display/slickText";
 import Switch from "@mui/material/Switch";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import { getCount, increment } from "../Transfer/Apis";
 import "./landing.css";
 
 export default function Landing() {
   const [checked, setChecked] = useState(false);
+  const [currentCount, setCurrentCount] = useState(null);
+
+  useEffect(() => {
+    getCount().then((response) => setCurrentCount(response.count));
+  }, []);
+
+  function handleIncrement() {
+    increment().then(() =>
+      getCount().then((response) => setCurrentCount(response.count))
+    );
+  }
 
   function handleSwitch(item) {
     setChecked(item);
@@ -22,49 +36,68 @@ export default function Landing() {
       >
         <Header />
 
-        <div className="panel"></div>
+        <Grid spacing="0" container>
+          <Grid item xs={3}>
+              <div
+                style={{
+                  margin: "0 auto",
+                  marginTop: "120px",
+                  width: "100%",
+                  textAlign: "center",
+                }}
+              >
+                <div style={{cursor: "pointer", textDecoration: "none"}} onClick={() => handleIncrement()}>
+                  <SlickText text="BUTTON" size="40px"/>
+                </div>
+                <SlickText text={currentCount} size="40px" />
+              </div>
+          </Grid>
 
-        <div className="frameBorder">
-          <div>
-            <div style={{ display: "inline-block" }}>
-              <SlickText text="Day" size="20px" />
-            </div>
-            <div
-              style={{
-                display: "inline-block",
-                position: "relative",
-                top: "-4px",
-              }}
-            >
-              <Switch
-                onChange={(event) => handleSwitch(event.target.checked)}
-              />
-            </div>
-            <div style={{ display: "inline-block" }}>
-              <SlickText text="Night" size="20px" />
-            </div>
-          </div>
+          <Grid item xs={6}>
+            <div className="frameBorder">
+              <div>
+                <div style={{ display: "inline-block" }}>
+                  <SlickText text="Day" size="20px" />
+                </div>
+                <div
+                  style={{
+                    display: "inline-block",
+                    position: "relative",
+                    top: "-4px",
+                  }}
+                >
+                  <Switch
+                    onChange={(event) => handleSwitch(event.target.checked)}
+                  />
+                </div>
+                <div style={{ display: "inline-block" }}>
+                  <SlickText text="Night" size="20px" />
+                </div>
+              </div>
 
-          <div className="frameBorderColour">
-            <iframe
-              src={`http://${
-                checked
-                  ? appConfig.cameraDetails.nightCameraURL
-                  : appConfig.cameraDetails.dayCameraURL
-              }/index.html`}
-              name="myiFrame"
-              scrolling="no"
-              frameborder="0"
-              marginheight="0px"
-              marginwidth="0px"
-              height="450px"
-              width="600px"
-              allowfullscreen
-            ></iframe>
-          </div>
-        </div>
+              <div className="frameBorderColour">
+                <iframe
+                  src={`http://${
+                    checked
+                      ? appConfig.cameraDetails.nightCameraURL
+                      : appConfig.cameraDetails.dayCameraURL
+                  }/index.html`}
+                  name="myiFrame"
+                  scrolling="no"
+                  frameBorder="0"
+                  marginLeft="50px"
+                  marginHeight="0px"
+                  marginWidth="0px"
+                  height="450px"
+                  width="600px"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+          </Grid>
 
-        <div className="panel"></div>
+          <Grid item xs={3}></Grid>
+        </Grid>
       </div>
     </>
   );
